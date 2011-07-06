da02745720caf839ed928392fdf291a73f4719c3
Kenley Cheung <ken@lordkenthegreat.com>
2011-07-06 16:21:31 -0400

    Refactoring overlay code.

--- chrome-extension/overlay.js
+++ chrome-extension/overlay.js
@@ -6,22 +6,24 @@
         $("#overlay").hide();
         // Create two padding divs.
         var padding = $('<div></div>');
-        padding.addClass("slide slide-normal slide-padding");
+        padding.addClass("slide slide-normal slide-filler slide-visible");
         padding.appendTo("#overlay");
         
         padding = $('<div></div>');
-        padding.addClass("slide slide-normal slide-padding");
+        padding.addClass("slide slide-normal slide-filler slide-visible");
         padding.appendTo("#overlay");
         
         var slide_count = 0;
-        for (slide in options["slides"])
+        var slides = options["slides"];
+        for (var i = 0; i < slides.length; i++)
         {
         	// Create a slide div for each slide passed into $().init().
         	var newdiv = $('<div></div>');
         	newdiv.attr("id", slide_count);
-        	newdiv.addClass("slide slide-normal");
+        	newdiv.addClass("slide slide-normal slide-visible");
         	var newimg = $('<img></img>');
-        	newimg.attr("src", slide);
+        	newimg.attr("src", slides[i]);
+        	newimg.addClass('slide-img');
         	newimg.appendTo(newdiv);
         	if (slide_count == 0)
         	{
@@ -30,10 +32,10 @@
         		newdiv.addClass("slide-focused");
         		$('#overlay').data('current_slide', slide_count);
         	}
-        	if (slide_count > 3)
+        	if (slide_count > 2)
         	{
-        		// Any slide beyond the fourth is hidden (for now)
-        		newdiv.hide();
+        		// Any slide beyond the third is hidden (for now)
+        		newdiv.css("display", "none");
         	}
         	newdiv.appendTo("#overlay");
         	slide_count++;
@@ -55,6 +57,12 @@
         var slide_count = $('#overlay').data('slide_count');
         if (current_slide != slide_count)
         {
+        	// Fade leftmost slide.
+        	$('#overlay .slide-visible:first').fadeOut("fast", function()
+        	{
+        		$('#overlay #' + (current_slide + 3).toString()).fadeIn("fast").removeClass("slide-hidden").addClass("slide-visible");
+        	}).removeClass("slide-visible").addClass("slide-hidden");
+        	// Move cursor.
         	$('#overlay #' + current_slide.toString()).removeClass("slide-focused");
         	$('#overlay #' + current_slide.toString()).addClass("slide-normal");
         	current_slide++;
@@ -89,3 +97,64 @@
 
   };
 })( jQuery );
+
+// Initialize keyboard + Kinect events and callbacks.
+$(function()
+{
+	$("body").keypress(function(e)
+	{
+		if (e.charCode == 111)
+		{
+			// Show overlay.
+			$().motion("show");
+		}
+		else if (e.charCode == 110)
+		{
+			// Proceed to next item.
+			$().motion("next");
+		}
+		else if (e.charCode == 112)
+		{
+			// Proceed to previous item.
+			$().motion("previous");
+		}
+		else if (e.charCode == 101)
+		{
+			// Hide overlay.
+			$().motion("hide");
+		}
+	});
+
+	DepthJS = {
+	      onKinectInit: function() {
+	        //
+	      },
+	      onRegister: function(x, y, z, data) {
+	        $().motion("show");
+	      },
+	      onUnregister: function() {
+	        $().motion("hide");
+	      },
+	      onMove: function(x, y, z) {
+	        //
+	      },
+	      onSwipeLeft: function() {
+	        $().motion("next");
+	      },
+	      onSwipeRight: function() {
+	        $().motion("previous");
+	      },
+	      onSwipeDown: function() {
+	        //
+	      },
+	      onSwipeUp: function() {
+	        //
+	      },
+	      onPush: function() {
+	        //
+	      },
+	      onPull: function() {
+	        //
+	      }
+	};
+});
